034ac5f7d6ebffae8ecf8f1116d3b546641408a1
Kate Klavina <kate.klavina97@gmail.com>
2019-11-11 10:29:33 +0200

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Dace Laizane on 11/11/2019.
+//  Copyright © 2019 Kate Klavina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
