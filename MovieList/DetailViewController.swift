8a7c9008c24b5179cf2c7e4ffaec7405f1e37042
Kate Klavina <kate.klavina97@gmail.com>
2019-11-11 16:04:59 +0200

    movieList

---
+++ MovieList/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  MovieList
+//
+//  Created by Dace Laizane on 11/11/2019.
+//  Copyright © 2019 Kate Klavina. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+   
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    
+    
+    @IBOutlet weak var movieNameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+//        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MovieList/MovieListTableViewController.swift
@@ -0,0 +1,110 @@
+//
+//  MovieListTableViewController.swift
+//  MovieList
+//
+//  Created by Dace Laizane on 11/11/2019.
+//  Copyright © 2019 Kate Klavina. All rights reserved.
+//
+
+import UIKit
+
+class MovieListTableViewController: UITableViewController {
+    
+    var movieList = [
+        "12 Angry Men (1957) - Action",
+        "Pulp Fiction (1994) - Action",
+        "Schindler's List (1993) - History",
+        "The Dark Knight (2008) - Action ",
+        "The Godfather (1972) - Drama",
+        "The Godfather- Part II (1974) - Drama",
+        "The Lord of the Rings- The Return of the King (2003) - Drama",
+        "The Shawshank Redemption (1994) - Drama"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // #warning Incomplete implementation, return the number of rows
+    //        return trackList.count
+            return movieList.count
+        }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        cell.textLabel?.text = movieList[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
+        cell.imageView?.image = UIImage(named: movieList[indexPath.row])
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+  
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+            
+            
+            let curentTrack = movieList.remove(at: fromIndexPath.row)
+            movieList.insert(curentTrack, at: to.row)
+            tableView.reloadData()
+
+        }
+        override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+            return .none
+        }
+    
+
+    
+    // MARK: - Navigation
+
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//     Get the new view controller using segue.destination.
+//     Pass the selected object to the new view controller.
+    if let indexPath = tableView.indexPathForSelectedRow{
+//         Get the new view controller using segue.destination.
+        let detailVC = segue.destination as! DetailViewController
+//         Pass the selected object to the new view controller.
+ //detailVC.song = songs[indexPath.row]
+       
+       }}
+
+}
