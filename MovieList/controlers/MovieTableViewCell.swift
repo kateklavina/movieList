f01ca8a7b3e96f384cca3b880a10da4f1c98d0a3
Kate Klavina <kate.klavina97@gmail.com>
2019-11-14 12:45:32 +0200

    no message

---
+++ MovieList/controlers/MovieTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieTableViewCell.swift
+//  MovieList
+//
+//  Created by Dace Laizane on 12/11/2019.
+//  Copyright © 2019 Kate Klavina. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieImageView: UIImageView!
+    
+    @IBOutlet weak var movieLabel: UILabel!
+}
